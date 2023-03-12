e0444acf3b84fb6e0ca028669c5fd5fb68325bcc
AninossII <aninosspro@gmail.com>
2023-03-12 01:19:58 +0000

    Make Open and CLose images :) trach

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,45 +1,63 @@
 #pragma warning(disable:4996)
 #include <iostream>
 
-int main()
+#define BMP_HEADER_SIZE			 54
+#define BMP_COLOR_TABLE_SIZE	1024
+#define CUSTOM_IMG_SIZE         1000*1000
+
+// Read from image
+void imgReader(const char* imgPath, int* _width, int* _height, int* _depth, unsigned char* _header, unsigned char* _colorTable, unsigned char* _buffer )
 {
+	FILE* fIn = fopen(imgPath, "rb");
+
+	if (fIn == (FILE*)0)
+		std::cout << "Unable to open image" << std::endl;
+
+	for (unsigned int i = 0; i < BMP_HEADER_SIZE; i++)
+		_header[i] = getc(fIn);
+
+	*_width = *(int*)&_header[18];
+	*_height = *(int*)&_header[22];
+	*_depth = *(int*) &_header[28];
+
+	if(*_depth <= 8)
+		fread(_colorTable, sizeof(unsigned char), BMP_COLOR_TABLE_SIZE, fIn);
+	fread(_buffer, sizeof(unsigned char), CUSTOM_IMG_SIZE, fIn);
+
+	fclose(fIn);
+}
+
+// Writing to image
+void imgWrite(const char* imgPath, int* _depth, unsigned char* _header, unsigned char* _colorTable, unsigned char* _buffer)
+{
+	FILE* fOut = fopen(imgPath, "wb");
+
+	fwrite(_header, sizeof(unsigned char), BMP_HEADER_SIZE, fOut);
+	if (*_depth <= 8)
+		fwrite(_colorTable, sizeof(unsigned char), BMP_COLOR_TABLE_SIZE, fOut);
+
+	fwrite(_buffer, sizeof(unsigned char), CUSTOM_IMG_SIZE, fOut);
+	fclose(fOut);
+}
+
+int main()
+	{
 	std::cout << "Hello World" << std::endl;
-	FILE* fIn = fopen("images/cameraman.bmp", "rb");
-	FILE* fOut = fopen("images/cameraMan_Copy.bmp", "wb");
+	int imgWidth, imgHeight, imgDepth;
+
+	unsigned char imgHeaderSize[BMP_HEADER_SIZE];
+	unsigned char imgColorTableSize[BMP_COLOR_TABLE_SIZE];
+	unsigned char imgBuffer[CUSTOM_IMG_SIZE];
+
+	const char imgName[] = "images/Clip Studio Pain.png";
+	const char newImgName[] = "images/Clip Studio Pain_copy.png";
 
 	// Reading from file
-	if (fIn == (FILE*)0)
-		std::cout << "Unable to open Image" << std::endl;
-
-	unsigned char header[54];
-	unsigned char colorTable[1024];
-
-	for (unsigned int i = 0; i < 54; i++)
-		header[i] = getc(fIn);
-
-	int size = *(int*)&header[2];
-	int width = *(int*)&header[18];
-	int height = *(int*)&header[22];
-	int depth = *(int*) & header[28];
-
-	if (depth <= 8)
-		fread(colorTable, sizeof(unsigned char), 1024, fIn);
+	imgReader(imgName, &imgWidth, &imgHeight, &imgDepth, &imgHeaderSize[0], &imgColorTableSize[0], &imgBuffer[0]);
 
 	// Writing to file
-	fwrite(header, sizeof(unsigned char), 54, fOut);
-	fwrite(colorTable, sizeof(unsigned char), 1024, fOut);
+	imgWrite(newImgName, &imgDepth, &imgHeaderSize[0], &imgColorTableSize[0], &imgBuffer[0]);
 
-	char* buff = new char[height * width];
-	fread(buff, sizeof(unsigned char), (width * height), fIn);
-	fwrite(buff, sizeof(unsigned char), (width * height), fOut);
-
-	fclose(fOut);
-	fclose(fIn);
-
-	std::cout << "Size: " << size << std::endl;
-	std::cout << "Width: " << width << std::endl;
-	std::cout << "Height: " << height << std::endl;
-	std::cout << "Depth: " << depth << std::endl;
 
 	return 0;
 }
---
+++ Project1/Project1/test.cpp
@@ -0,0 +1,14 @@
+// #pragma warning(disable:4996)
+// #include <iostream>
+//
+// int main()
+// {
+// 	int i = 1;
+// 	int* j = &i;
+// 	std::cout << "Number i = " << i << std::endl;
+// 	std::cout << "Number i Reference  = " << &i << std::endl;
+// 	std::cout << "Number j = " << j << std::endl;
+// 	std::cout << "Number j Pointer = " << *j << std::endl;
+// 	std::cout << "Number j Reference = " << &j << std::endl;
+// 	return 0;
+// }
